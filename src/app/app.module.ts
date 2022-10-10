16ca9f36ee433daf177235c57394be83e3d87d9d
ungerecht <kevinungerecht@gmail.com>
2022-10-09 18:50:40 -0700

    add material button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { ReportsComponent } from './reports/reports.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, InventoryComponent, ReportsComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    InventoryComponent,
+    ReportsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
