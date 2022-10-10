b8d92452dc388dc18a12bfe7090aa43438cc126b
ungerecht <kevinungerecht@gmail.com>
2022-10-09 22:38:59 -0700

    use mock inventory, create table columns

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Item } from '../Item';
+import { inventory } from '../mock-inventory';
+
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.css']
+  styleUrls: ['./inventory.component.css'],
 })
 export class InventoryComponent implements OnInit {
+  title = 'Inventory';
+  displayedColumns: string[] = ['name', 'description', 'quantity', 'delete'];
+  inventory: Item[] = inventory;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  log(message: string): void {
+    console.log(message);
   }
-
 }
