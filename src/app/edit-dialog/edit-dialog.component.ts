589c6595819ead8cad36eb794051f50cc6aee5fe
ungerecht <kevinungerecht@gmail.com>
2022-10-10 02:16:52 -0700

    inject item data

--- src/app/edit-dialog/edit-dialog.component.ts
+++ src/app/edit-dialog/edit-dialog.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-edit-dialog',
   templateUrl: './edit-dialog.component.html',
-  styleUrls: ['./edit-dialog.component.css']
+  styleUrls: ['./edit-dialog.component.css'],
 })
-export class EditDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class EditDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Item) {}
 }
