0739146b2d6993037b9403c1f8e1a6d39c8afae7
ungerecht <kevinungerecht@gmail.com>
2022-10-09 17:52:49 -0700

    generate dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
