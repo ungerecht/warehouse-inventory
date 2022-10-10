3385857c353c37d9951c13c605fad926eebf3840
ungerecht <kevinungerecht@gmail.com>
2022-10-09 18:16:38 -0700

    generate reports component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InventoryComponent } from './inventory/inventory.component';
+import { ReportsComponent } from './reports/reports.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, InventoryComponent],
+  declarations: [AppComponent, DashboardComponent, InventoryComponent, ReportsComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
