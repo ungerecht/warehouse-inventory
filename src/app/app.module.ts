6f336cea3097f9a67e93936bfb442c925234aba5
ungerecht <kevinungerecht@gmail.com>
2022-10-09 18:05:24 -0700

    generate inventory component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
+  declarations: [AppComponent, DashboardComponent, InventoryComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
