22f74a96c7316ffd8cd4e09385120eb4bdc7b49a
ungerecht <kevinungerecht@gmail.com>
2022-10-09 22:37:30 -0700

    create mock inventory

---
+++ src/app/mock-inventory.ts
@@ -0,0 +1,22 @@
+import { Item } from './Item';
+
+export const inventory: Item[] = [
+  { id: 1, name: 'onions', description: 'sweet', quantity: 10, unit: 'lbs' },
+  { id: 2, name: 'peppers', description: 'red', quantity: 5, unit: 'lbs' },
+  { id: 3, name: 'beans', description: 'black', quantity: 12, unit: 'cans' },
+  {
+    id: 4,
+    name: 'jalapenos',
+    description: 'spicy',
+    quantity: 100,
+    unit: 'lb',
+  },
+  {
+    id: 5,
+    name: 'cheese',
+    description: 'shredded',
+    quantity: 6000,
+    unit: 'lb',
+  },
+  { id: 1, name: 'tortillas', description: 'corn', quantity: 69, unit: '' },
+];
