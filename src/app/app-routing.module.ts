669177ad12d7f2da7d69aa551ccd60af4dc9cfd0
ungerecht <kevinungerecht@gmail.com>
2022-10-09 18:07:18 -0700

    create inventory route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,7 @@ const routes: Routes = [
     path: 'dashboard',
     component: DashboardComponent,
   },
+  { path: 'inventory', component: InventoryComponent },
 ];
 
 @NgModule({
