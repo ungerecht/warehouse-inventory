295f89afb1b5cfe33926fcae3da3d3e9874e3c17
ungerecht <kevinungerecht@gmail.com>
2022-10-10 18:30:05 -0700

    inject item data

--- src/app/send-dialog/send-dialog.component.ts
+++ src/app/send-dialog/send-dialog.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-send-dialog',
   templateUrl: './send-dialog.component.html',
-  styleUrls: ['./send-dialog.component.css']
+  styleUrls: ['./send-dialog.component.css'],
 })
-export class SendDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SendDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Item) {}
 }
