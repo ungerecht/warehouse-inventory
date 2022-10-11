9dbc0e1316a700a7634bdc172145addcebd75754
ungerecht <kevinungerecht@gmail.com>
2022-10-10 18:36:02 -0700

    inject item data

--- src/app/delete-dialog/delete-dialog.component.ts
+++ src/app/delete-dialog/delete-dialog.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-delete-dialog',
   templateUrl: './delete-dialog.component.html',
-  styleUrls: ['./delete-dialog.component.css']
+  styleUrls: ['./delete-dialog.component.css'],
 })
-export class DeleteDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DeleteDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Item) {}
 }
