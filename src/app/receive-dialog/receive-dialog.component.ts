f72ec17bd08c17e8c977b2534fe5c3dcd5d8b1f8
ungerecht <kevinungerecht@gmail.com>
2022-10-10 18:21:16 -0700

    inject item data

--- src/app/receive-dialog/receive-dialog.component.ts
+++ src/app/receive-dialog/receive-dialog.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-receive-dialog',
   templateUrl: './receive-dialog.component.html',
-  styleUrls: ['./receive-dialog.component.css']
+  styleUrls: ['./receive-dialog.component.css'],
 })
-export class ReceiveDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ReceiveDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Item) {}
 }
