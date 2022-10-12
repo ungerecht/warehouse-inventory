c04005098756af7b056d87fb78f4a73facba35ce
ungerecht <kevinungerecht@gmail.com>
2022-10-11 21:55:31 -0700

    use dialogRef, send data back to inventory

--- src/app/send-dialog/send-dialog.component.ts
+++ src/app/send-dialog/send-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Item } from '../item';
 
 @Component({
@@ -8,5 +8,24 @@ import { Item } from '../item';
   styleUrls: ['./send-dialog.component.css'],
 })
 export class SendDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Item) {}
+  item: Item = { ...this.data };
+  sendQuantity: number = 1;
+  date: string = new Date()
+    .toLocaleString('sv', { timeZoneName: 'short' })
+    .split(' ')[0];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Item,
+    private dialogRef: MatDialogRef<SendDialogComponent>
+  ) {}
+
+  send() {
+    if (this.item.quantity >= this.sendQuantity || this.sendQuantity < 1) {
+      (this.item.quantity = this.item.quantity - this.sendQuantity),
+        //TODO handle date
+        this.dialogRef.close(this.item);
+    } else {
+      //error, sent quanitity must be between 1 and current quantity
+    }
+  }
 }
