7f52207ff1a75cb08928e2ee4bafbcf9806d01e2
ungerecht <kevinungerecht@gmail.com>
2022-10-11 21:52:22 -0700

    use dialogRef, pass data back to inventory

--- src/app/add-dialog/add-dialog.component.ts
+++ src/app/add-dialog/add-dialog.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-add-dialog',
   templateUrl: './add-dialog.component.html',
-  styleUrls: ['./add-dialog.component.css']
+  styleUrls: ['./add-dialog.component.css'],
 })
-export class AddDialogComponent implements OnInit {
+export class AddDialogComponent {
+  name: string = '';
+  description: string = '';
+  quantity: number = 0;
+  unit: string = '';
+  date: string = new Date()
+    .toLocaleString('sv', { timeZoneName: 'short' })
+    .split(' ')[0];
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<AddDialogComponent>) {}
 
-  ngOnInit(): void {
+  add() {
+    const data = {
+      name: this.name,
+      description: this.description,
+      quantity: this.quantity,
+      unit: this.unit,
+    };
+    //TODO handle date
+    this.dialogRef.close(data);
   }
-
 }
