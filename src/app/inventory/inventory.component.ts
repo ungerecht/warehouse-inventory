5fc33fa1dc6bf81d6f1abbd8390074e2551ceb2d
ungerecht <kevinungerecht@gmail.com>
2022-10-11 21:56:23 -0700

    use item service, create functions for dialog actions

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -5,6 +5,7 @@ import { MatTableDataSource } from '@angular/material/table';
 
 import { Item } from '../item';
 import { inventory } from '../mock-inventory';
+import { ItemService } from '../item.service';
 
 import { AddDialogComponent } from '../add-dialog/add-dialog.component';
 import { EditDialogComponent } from '../edit-dialog/edit-dialog.component';
@@ -24,36 +25,100 @@ export class InventoryComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(public dialog: MatDialog) {}
+  constructor(private itemService: ItemService, public dialog: MatDialog) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getItems();
+  }
 
   ngAfterViewInit(): void {
     this.inventory.sort = this.sort;
+  }
+
+  getItems(): void {
+    this.itemService
+      .getItems()
+      .subscribe((items) => (this.inventory.data = items));
+  }
+
+  add(item: Item): void {
+    item.name = item.name.trim();
+    if (!item) {
+      return;
+    }
+    this.itemService.addItem(item).subscribe(() => {
+      this.getItems();
+    });
+  }
+
+  edit(item: Item): void {
+    this.itemService.updateItem(item).subscribe(() => {
+      this.getItems();
+    });
+  }
+
+  delete(item: Item): void {
+    this.itemService.deleteItem(item.id).subscribe(() => {
+      this.getItems();
+    });
+  }
+
+  receive(item: Item): void {
+    this.itemService.updateItem(item).subscribe(() => {
+      this.getItems();
+    });
+    //TODO record new receive transaction
+  }
+
+  send(item: Item): void {
+    this.itemService.updateItem(item).subscribe(() => {
+      this.getItems();
+    });
+    //TODO record new send transaction
   }
 
   filter(term: string): void {
     this.inventory.filter = term.trim().toLocaleLowerCase();
   }
 
-  openEditDialog(data: Item) {
-    this.dialog.open(EditDialogComponent, { data });
+  openAddDialog() {
+    const dialogRef = this.dialog.open(AddDialogComponent, {});
+
+    dialogRef.afterClosed().subscribe((data) => {
+      if (data) this.add(data as Item);
+    });
   }
 
-  openAddDialog() {
-    this.dialog.open(AddDialogComponent, {});
+  openEditDialog(data: Item) {
+    const dialogRef = this.dialog.open(EditDialogComponent, { data });
+
+    dialogRef.afterClosed().subscribe((data) => {
+      if (data) this.edit(data);
+    });
   }
 
   openReceiveDialog(data: Item) {
-    this.dialog.open(ReceiveDialogComponent, { data });
+    const dialogRef = this.dialog.open(ReceiveDialogComponent, { data });
+
+    dialogRef.afterClosed().subscribe((data) => {
+      this.receive(data);
+    });
   }
 
   openSendDialog(data: Item) {
-    this.dialog.open(SendDialogComponent, { data });
+    const dialogRef = this.dialog.open(SendDialogComponent, { data });
+
+    dialogRef.afterClosed().subscribe((data) => {
+      this.send(data);
+    });
   }
 
   openDeleteDialog(data: Item) {
-    this.dialog.open(DeleteDialogComponent, { data });
+    const dialogRef = this.dialog.open(DeleteDialogComponent, { data });
+
+    dialogRef.afterClosed().subscribe((data) => {
+      this.delete(data);
+    });
   }
 
   log(message: string): void {
