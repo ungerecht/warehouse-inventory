73a92ae26b8098003fa6eabbaa91bbbe658fc02b
ungerecht <kevinungerecht@gmail.com>
2022-10-11 21:55:23 -0700

    use dialogRef, send data back to inventory

--- src/app/receive-dialog/receive-dialog.component.ts
+++ src/app/receive-dialog/receive-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Item } from '../item';
 
 @Component({
@@ -8,5 +8,25 @@ import { Item } from '../item';
   styleUrls: ['./receive-dialog.component.css'],
 })
 export class ReceiveDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Item) {}
+  item: Item = { ...this.data };
+  receiveQuantity: number = 1;
+  date: string = new Date()
+    .toLocaleString('sv', { timeZoneName: 'short' })
+    .split(' ')[0];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Item,
+    private dialogRef: MatDialogRef<ReceiveDialogComponent>
+  ) {}
+
+  receive() {
+    if (this.receiveQuantity >= 0) {
+      this.item.quantity =
+        Number(this.item.quantity) + Number(this.receiveQuantity);
+      //TODO handle date
+      this.dialogRef.close(this.item);
+    } else {
+      //error, receive quanitity must be greater than 0
+    }
+  }
 }
