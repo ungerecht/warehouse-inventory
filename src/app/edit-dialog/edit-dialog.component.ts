25468248e4daae6c515b0bb8a4de3c541a019933
ungerecht <kevinungerecht@gmail.com>
2022-10-12 21:04:17 -0700

    validate dialog input

--- src/app/edit-dialog/edit-dialog.component.ts
+++ src/app/edit-dialog/edit-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Item } from '../item';
 
@@ -8,7 +9,9 @@ import { Item } from '../item';
   styleUrls: ['./edit-dialog.component.css'],
 })
 export class EditDialogComponent {
-  edited = { ...this.data };
+  name = new FormControl(this.data.name, [Validators.required]);
+  description: string = this.data.description;
+  unit: string = this.data.unit;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: Item,
@@ -16,6 +19,12 @@ export class EditDialogComponent {
   ) {}
 
   save() {
-    this.dialogRef.close(this.edited);
+    if (this.name.value!.length > 0) {
+      const edited = { ...this.data };
+      edited.name = this.name.value!;
+      edited.description = this.description;
+      edited.unit = this.unit;
+      this.dialogRef.close(edited);
+    }
   }
 }
