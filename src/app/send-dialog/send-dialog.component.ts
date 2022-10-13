e8d1fe313919e3ae29e25d781de59817e0623bc1
ungerecht <kevinungerecht@gmail.com>
2022-10-12 21:23:25 -0700

    validate send dialog input

--- src/app/send-dialog/send-dialog.component.ts
+++ src/app/send-dialog/send-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Item } from '../item';
 
@@ -8,11 +9,14 @@ import { Item } from '../item';
   styleUrls: ['./send-dialog.component.css'],
 })
 export class SendDialogComponent {
-  item: Item = { ...this.data };
-  sendQuantity: number = 1;
-  date: string = new Date()
-    .toLocaleString('sv', { timeZoneName: 'short' })
-    .split(' ')[0];
+  sendQuantity = new FormControl(1, [
+    Validators.min(1),
+    Validators.max(this.data.quantity),
+  ]);
+  date = new FormControl(
+    new Date().toLocaleString('sv', { timeZoneName: 'short' }).split(' ')[0],
+    [Validators.required]
+  );
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: Item,
@@ -20,12 +24,20 @@ export class SendDialogComponent {
   ) {}
 
   send() {
-    if (this.item.quantity >= this.sendQuantity || this.sendQuantity < 1) {
-      (this.item.quantity = this.item.quantity - this.sendQuantity),
-        //TODO handle date
-        this.dialogRef.close(this.item);
-    } else {
-      //error, sent quanitity must be between 1 and current quantity
+    if (
+      this.sendQuantity.value! <= this.data.quantity &&
+      this.sendQuantity.value! > 0 &&
+      Date.parse(this.date.value!)
+    ) {
+      const data = {
+        id: this.data.id,
+        name: this.data.name,
+        description: this.data.description,
+        quantity: this.data.quantity - this.sendQuantity.value!,
+        unit: this.data.unit,
+        date: this.date.value,
+      };
+      this.dialogRef.close(data);
     }
   }
 }
