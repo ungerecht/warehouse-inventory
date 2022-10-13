58a5823390bd3f3d4448cdf6c35fe9fed26d1f5c
ungerecht <kevinungerecht@gmail.com>
2022-10-13 02:57:40 -0700

    create database for transactions

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Item } from './item';
+import { Transaction } from './transaction';
 
 @Injectable({
   providedIn: 'root',
@@ -41,11 +42,74 @@ export class InMemoryDataService implements InMemoryDbService {
         id: 6,
         name: 'tortillas',
         description: 'flour',
-        quantity: 69,
+        quantity: 203,
         unit: '',
       },
     ];
-    return { items };
+    const transactions = [
+      {
+        id: 1,
+        itemid: 1,
+        date: '2022-10-10',
+        amount: 5,
+      },
+      {
+        id: 2,
+        itemid: 1,
+        date: '2022-10-11',
+        amount: 5,
+      },
+      {
+        id: 3,
+        itemid: 2,
+        date: '2022-10-11',
+        amount: 5,
+      },
+      {
+        id: 4,
+        itemid: 3,
+        date: '2022-10-11',
+        amount: 12,
+      },
+      {
+        id: 5,
+        itemid: 4,
+        date: '2022-10-11',
+        amount: 75,
+      },
+      {
+        id: 6,
+        itemid: 5,
+        date: '2022-10-11',
+        amount: 7000,
+      },
+      {
+        id: 7,
+        itemid: 6,
+        date: '2022-10-11',
+        amount: 300,
+      },
+      {
+        id: 8,
+        itemid: 6,
+        date: '2022-10-12',
+        amount: -97,
+      },
+      {
+        id: 8,
+        itemid: 5,
+        date: '2022-10-12',
+        amount: -1000,
+      },
+      {
+        id: 9,
+        itemid: 4,
+        date: '2022-10-13',
+        amount: 25,
+      },
+    ];
+
+    return { items, transactions };
   }
 
   // Overrides the genId method to ensure that an item always has an id.
@@ -53,7 +117,7 @@ export class InMemoryDataService implements InMemoryDbService {
   // the method below returns the initial number (1).
   // if the items array is not empty, the method below returns the highest
   // item id + 1.
-  genId(items: Item[]): number {
-    return items.length > 0 ? Math.max(...items.map((item) => item.id)) + 1 : 1;
+  genId<T extends Item | Transaction>(table: T[]): number {
+    return table.length > 0 ? Math.max(...table.map((t) => t.id)) + 1 : 1;
   }
 }
